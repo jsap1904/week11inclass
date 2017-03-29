de59afc17f2a81f58e04780a2c735e66aa84350d
jsap1904 <sapinoso.j@gmail.com>
2017-03-28 18:55:14 -0700

    update

--- js/main.js
+++ js/main.js
@@ -19,3 +19,27 @@
 
 // var array = [ 1,true, "hi"]
 // 	console.log(array.reverse())
+
+// var stringArray = ["pepperoni", "sausage", "ham"]
+// 	console.log(stringArray.sort())
+// 	stringArray.push("mushroom")
+// 	console.log(stringArray)
+
+// var numArray = [ 5,66,8768,425436 ]
+// 	console.log(numArray.sort( function(a,b){return a-b} ))
+// 	numArray.pop()
+// 	console.log(numArray)
+
+function trans (name,cost,seats) {
+	this.name= name
+	this.cost = cost
+	this.seats = seats
+}
+
+var bart = new trans('Bart', 5.80, 120)
+var muni = new trans('Muni',2.25, 50)
+var trolley = new trans('Trolley', 7, 25)
+
+console.log(bart)
+console.log(muni)
+console.log(trolley)
