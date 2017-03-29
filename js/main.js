69638b11a7a3959f80474ae1b39471b44897d8e3
jsap1904 <sapinoso.j@gmail.com>
2017-03-28 18:28:39 -0700

    upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// var boolean = true
+// 	console.log(boolean.length)
+
+// var num = (1,2,3)
+// 	console.log(num.length)
+
+// var string = "hello world"
+// 	console.log(string.length)
+
+// var array = [ 1,true, "hi"]
+// 	console.log(array.length)
+
+// var object = {
+// 	car: "honda",
+// 	model: "s2000",
+// 	year: "2007"
+// }
+// 	console.log(object.length)
+
+// var array = [ 1,true, "hi"]
+// 	console.log(array.reverse())
